001ede2b4bd7e5c4c9d4f32ffab4f65b3517a3cf
Stephan Deublein <stephan.deublein@mv.uni-kl.de>
2009-11-12 10:00:02 +0000

    BugFix Ewald-Summation Gradual Insertion - Remove Switch to Extended Reaction Field
    git-svn-id: https://scm.projects.hlrs.de/authscm/sleyh/svn/ms2/branches/InnereFG@55 a2be33b2-7eaf-4dce-bf61-b02fde6c56c2

--- src/ms2_ensemble.F90
+++ src/ms2_ensemble.F90
@@ -4357,9 +4357,6 @@ loop3:    do nc = 1, this%NComponents
 
        ! Calculation of Chemical Potential for Ionic systems
         if (LongRange .eq. Ewald) then
-          ewald_h = 1
-          ! Set LongRange Correction to Extended ReactionField
-          LongRange = 4
           call CalculateCorr (this)
 #if MPI_VER > 0
           call Energy( this, EPot_h )
