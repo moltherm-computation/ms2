e5f770d508b2a6f01fac8ec316899aed0feafe3e
Martin Bernreuther <bernreuther@hlrs.de>
2010-07-30 11:20:00 +0000

    ms2_stopwatch.F90: change the default to use old MPI1 "include" mechanism (for now!) MPI3 will only require the mpi Fortran module - the "include"s will be depricated...
    git-svn-id: https://scm.projects.hlrs.de/authscm/sleyh/svn/ms2/trunk@179 a2be33b2-7eaf-4dce-bf61-b02fde6c56c2

--- src/ms2_stopwatch.F90
+++ src/ms2_stopwatch.F90
@@ -34,7 +34,10 @@
 
 #if MPI_VER
 #define USE_MPI
-#define MPI_USE_MODULE
+! the MPI2 standard requires the support of a mpi module (see 16.2),
+! but some MPI1 distributions don't offer a Fortran90 binding and won't compile
+! uncomment the next line, if you have a MPI distribution for MPI>=version 2
+!#define MPI_USE_MODULE
 #define STOPWATCH_USE_MPIWTIME
 #endif
 
