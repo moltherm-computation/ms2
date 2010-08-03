ac8f012771c6fabae7a366c3d8cb47a0cd0d2238
Colin Glass <glass@hlrs.de>
2010-08-03 14:16:05 +0000

    Catch running TRANS code without calculating transport properties
    git-svn-id: https://scm.projects.hlrs.de/authscm/sleyh/svn/ms2/trunk@180 a2be33b2-7eaf-4dce-bf61-b02fde6c56c2

--- src/ms2_simulation.F90
+++ src/ms2_simulation.F90
@@ -638,6 +638,8 @@ contains
     case( 'no', 'nein' )
       CorrfunMode = inactive
       CorrfunModeString = 'No transport properties'
+      call Error( 'Use a binary compiled without -DTRANS if you do not &
+& wish to calculate transport properties. If you do, set CorrFunMode = yes ' )
     case default
       call Error( 'Unknown transport properties ('//trim(IdCorrFun)//'='//trim(str)//')' )
     end select
