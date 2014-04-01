2bf7f67169838db03b5d08c5da21d7e19629118d
Andreas Köster <koester2@mail.upb.de>
2014-04-01 18:39:44 +0000

    Test suite files part 1 (MC_GE, MC_NVT, MC_NPT, RDF and SVC) added. Result files will follow...
    git-svn-id: https://scm.projects.hlrs.de/authscm/sleyh/svn/ms2/trunk@291 a2be33b2-7eaf-4dce-bf61-b02fde6c56c2

---
+++ test/RDF/HClpc.pm
@@ -0,0 +1,41 @@
+NSiteTypes = 2
+
+SiteType   = LJ126
+NSites     = 2
+
+# HCl 
+x          =  	 0.000000 
+y          =  	 0.000000
+z          =  	 0.000000
+sigma      =      3.52 
+epsilon    =    179.
+mass       =     35.45
+
+x          =   0.0
+y          =   0.0
+z          =   1.28
+sigma      =   0.00000000000000001
+epsilon    =   0.00000000000000001
+mass       =   0.00000000000000001
+
+ 
+
+SiteType   = Charge
+NSites     = 2
+
+x          =   0.0
+y          =   0.0
+z          =   1.28
+charge     =   0.2731
+mass       =   1.08
+shielding  =   0.5
+
+x          =   0.0 
+y          =   0.0
+z          =   0.0
+charge     =  -0.2731
+mass       =   0.0
+shielding  =   0.5
+
+
+NRotAxes   = auto
