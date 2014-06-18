2050b2f03ff82529893df01ed44bc3308c8adfc2
Andreas Köster <koester2@mail.upb.de>
2014-06-18 10:37:11 +0000

    Test suite update
    git-svn-id: https://scm.projects.hlrs.de/authscm/sleyh/svn/ms2/trunk@323 a2be33b2-7eaf-4dce-bf61-b02fde6c56c2

---
+++ test/NPT_part_molar/lj_B.pm
@@ -0,0 +1,14 @@
+NSiteTypes =   1
+
+SiteType   =   LJ126
+NSites     =   1
+
+# NH3-site (united-atom)
+x          =   0.0
+y          =   0.0
+z          =   0.0
+sigma      =   1.0
+epsilon    =   0.7
+mass       =   1.0
+
+NRotAxes   =   auto
