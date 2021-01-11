8a9615ebb54b4781d92eb42d12710aef9adcb65b
Robin Fingerhut <robin.fingerhut@tu-berlin.de>
2021-01-11 15:21:49 +0000

    4.000 Set ms2 version to 4.000 which will be published.
    git-svn-id: https://scm.projects.hlrs.de/authscm/sleyh/svn/ms2/trunk@658 a2be33b2-7eaf-4dce-bf61-b02fde6c56c2

--- src/ms2_version.F90
+++ src/ms2_version.F90
@@ -10,7 +10,7 @@
 
 module ms2_version
 
-  character(*), parameter :: VersionString = '3.081'
-  real(4)                :: ms2VersionNr = 3.081
+  character(*), parameter :: VersionString = '4.000'
+  real(4)                :: ms2VersionNr = 4.000
 
 end module ms2_version
