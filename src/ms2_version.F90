73b035b71c53f29eca37324e7a12532c993aded4
Sascha Leyh <sascha.leyh@tu-berlin.de>
2021-01-22 11:52:24 +0000

    4.001 Add hint for fftw3 library to main makefile
    On systems which does not have the library installed or the module is not loaded, this hint might help.
    
    git-svn-id: https://scm.projects.hlrs.de/authscm/sleyh/svn/ms2/trunk@659 a2be33b2-7eaf-4dce-bf61-b02fde6c56c2

--- src/ms2_version.F90
+++ src/ms2_version.F90
@@ -10,7 +10,7 @@
 
 module ms2_version
 
-  character(*), parameter :: VersionString = '4.000'
-  real(4)                :: ms2VersionNr = 4.000
+  character(*), parameter :: VersionString = '4.001'
+  real(4)                :: ms2VersionNr = 4.001
 
 end module ms2_version
