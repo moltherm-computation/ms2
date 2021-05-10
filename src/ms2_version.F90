659e3789c2eeda716b03de533cf5abd314971e23
Robin Fingerhut <robin.fingerhut@tu-berlin.de>
2021-05-10 08:36:38 +0000

    4.013 In MC, pressure correction with ReactionField does not need the selfterm...

--- src/ms2_version.F90
+++ src/ms2_version.F90
@@ -10,7 +10,7 @@
 
 module ms2_version
 
-  character(*), parameter :: VersionString = '4.012'
-  real(4)                :: ms2VersionNr = 4.012
+  character(*), parameter :: VersionString = '4.013'
+  real(4)                :: ms2VersionNr = 4.013
 
 end module ms2_version
