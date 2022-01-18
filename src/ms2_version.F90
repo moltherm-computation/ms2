f1cfb68e656c147c4c5f0e8ddbf294de40fc8a77
Robin Fingerhut <robin.fingerhut@tu-berlin.de>
2022-01-18 10:02:07 +0100

    4.030 Bugfix MUVT ErrorsUpdate; done for Isabel Nitzke.

--- src/ms2_version.F90
+++ src/ms2_version.F90
@@ -10,7 +10,7 @@
 
 module ms2_version
 
-  character(*), parameter :: VersionString = '4.029'
-  real(4)                :: ms2VersionNr = 4.029
+  character(*), parameter :: VersionString = '4.030'
+  real(4)                :: ms2VersionNr = 4.030
 
 end module ms2_version
