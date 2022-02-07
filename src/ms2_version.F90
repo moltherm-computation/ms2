882d53245c97b57a9d3712a6ee996d81732d0a14
Christoph Niethammer <niethammer@hlrs.de>
2022-02-07 16:05:48 +0100

    Fix missing status parameter for MPI_Recv calls

--- src/ms2_version.F90
+++ src/ms2_version.F90
@@ -10,7 +10,7 @@
 
 module ms2_version
 
-  character(*), parameter :: VersionString = '4.041'
-  real(4)                :: ms2VersionNr = 4.041
+  character(*), parameter :: VersionString = '4.042'
+  real(4)                :: ms2VersionNr = 4.042
 
 end module ms2_version
