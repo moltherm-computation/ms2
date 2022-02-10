480ec8a5f68cc56c1bc38d64c33beec2b65dda1c
Sascha Leyh <sascha.leyh@tu-berlin.de>
2022-02-10 06:52:38 +0100

    Revise *.run and *.rav header writing

--- src/ms2_version.F90
+++ src/ms2_version.F90
@@ -10,7 +10,7 @@
 
 module ms2_version
 
-  character(*), parameter :: VersionString = '4.049'
-  real(4)                :: ms2VersionNr = 4.049
+  character(*), parameter :: VersionString = '4.050'
+  real(4)                :: ms2VersionNr = 4.050
 
 end module ms2_version
