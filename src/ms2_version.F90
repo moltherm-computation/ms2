7e68f88b2c0bc9ca99ea608c9bcb389645360b7e
Robin Fingerhut <robin.fingerhut@tu-berlin.de>
2022-03-07 13:46:19 +0100

    4.058 Bugfix TRANS=1 array Tau in accumulator

--- src/ms2_version.F90
+++ src/ms2_version.F90
@@ -10,7 +10,7 @@
 
 module ms2_version
 
-  character(*), parameter :: VersionString = '4.057'
-  real(4)                :: ms2VersionNr = 4.057
+  character(*), parameter :: VersionString = '4.058'
+  real(4)                :: ms2VersionNr = 4.058
 
 end module ms2_version
