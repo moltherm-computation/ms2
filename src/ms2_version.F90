9e914092a2cab040e5df665216cc359f6da22b27
Sascha Leyh <sascha.leyh@tu-berlin.de>
2022-08-19 06:26:22 +0200

    001 Add plot for reference comparison to validation case pentan_1

--- src/ms2_version.F90
+++ src/ms2_version.F90
@@ -10,7 +10,7 @@
 
 module ms2_version
 
-  character(*), parameter :: VersionString = '4.084'
+  character(*), parameter :: VersionString = '4.084_pentan_001'
   real(4)                :: ms2VersionNr = 4.084
 
 end module ms2_version
