5d1caea0bb4eacde44dd3530534754970430613a
Sascha Leyh <sascha.leyh@tu-berlin.de>
2022-09-02 15:33:47 +0200

    001 Add plots to validation case

--- src/ms2_global.F90
+++ src/ms2_global.F90
@@ -1226,7 +1226,7 @@ contains
   subroutine Global_printVersion()
     implicit none
     if (RootProc) then
-      print *, trim( ProgramFileName ), ' Version: ', VersionString, "_idf" &
+      print *, trim( ProgramFileName ), ' Version: ', VersionString &
 &            , ' (compiled at ', CompileTime, ')'
     end if
   end subroutine Global_printVersion
