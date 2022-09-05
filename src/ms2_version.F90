9f4d18d64cb47839f67a38970641bf2dc548ceef
Sascha Leyh <sascha.leyh@tu-berlin.de>
2022-09-05 08:28:24 +0200

    003 Correct state point and revise plot script

--- src/ms2_version.F90
+++ src/ms2_version.F90
@@ -10,7 +10,7 @@
 
 module ms2_version
 
-  character(*), parameter :: VersionString = '4.084_propanol_002'
+  character(*), parameter :: VersionString = '4.084_propanol_003'
   real(4)                :: ms2VersionNr = 4.084
 
 end module ms2_version
