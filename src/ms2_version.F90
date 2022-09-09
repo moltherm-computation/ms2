42eb5ad25ed2b010723645b95c5604fdce7bc108
Sascha Leyh <sascha.leyh@tu-berlin.de>
2022-09-09 14:42:05 +0200

    010 Add plots to other IFG validation cases

--- src/ms2_version.F90
+++ src/ms2_version.F90
@@ -10,7 +10,7 @@
 
 module ms2_version
 
-  character(*), parameter :: VersionString = '4.084_idf_009'
+  character(*), parameter :: VersionString = '4.084_idf_010'
   real(4)                :: ms2VersionNr = 4.084
 
 end module ms2_version
