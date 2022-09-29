9a63b54f88359bbf501da492ca5ae84a4e32c107
Sascha Leyh <sascha.leyh@tu-berlin.de>
2022-09-29 10:45:32 +0200

    Update pipeline for new machine

--- src/ms2_version.F90
+++ src/ms2_version.F90
@@ -10,7 +10,7 @@
 
 module ms2_version
 
-  character(*), parameter :: VersionString = '4.085'
-  real(4)                :: ms2VersionNr = 4.085
+  character(*), parameter :: VersionString = '4.086'
+  real(4)                :: ms2VersionNr = 4.086
 
 end module ms2_version
