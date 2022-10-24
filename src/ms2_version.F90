5e67feb503148774c09c73996bbc9c209b72b1e9
Isabel Nitzke <nitzke@tu-berlin.de>
2022-10-24 12:47:35 +0200

    Fix isobaric partition function

--- src/ms2_version.F90
+++ src/ms2_version.F90
@@ -10,7 +10,7 @@
 
 module ms2_version
 
-  character(*), parameter :: VersionString = '4.092'
-  real(4)                :: ms2VersionNr = 4.092
+  character(*), parameter :: VersionString = '4.093'
+  real(4)                :: ms2VersionNr = 4.093
 
 end module ms2_version
