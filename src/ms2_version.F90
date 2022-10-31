0ffd35172e14fbaa7b4a8a758fe83faa699ceb14
Sascha Leyh <sascha.leyh@tu-berlin.de>
2022-10-31 09:21:32 +0100

    Combine move and rotate in one routine

--- src/ms2_version.F90
+++ src/ms2_version.F90
@@ -10,7 +10,7 @@
 
 module ms2_version
 
-  character(*), parameter :: VersionString = '4.096'
-  real(4)                :: ms2VersionNr = 4.096
+  character(*), parameter :: VersionString = '4.097'
+  real(4)                :: ms2VersionNr = 4.097
 
 end module ms2_version
