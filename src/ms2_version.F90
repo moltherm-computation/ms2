d7882d7709a0033cd822e0d78d27bf23fe1393fa
Isabel Nitzke <nitzke@tu-berlin.de>
2024-09-19 13:09:51 +0200

    4.113 Change mue to MU in par

--- src/ms2_version.F90
+++ src/ms2_version.F90
@@ -10,7 +10,7 @@
 
 module ms2_version
 
-  character(*), parameter :: VersionString = '4.112'
-  real(4)                :: ms2VersionNr = 4.112
+  character(*), parameter :: VersionString = '4.113'
+  real(4)                :: ms2VersionNr = 4.113
 
 end module ms2_version 
