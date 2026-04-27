5c441787aaddc6b9e337b5877e0a6b206aa7b6d0
Isabel Nitzke <isabel.nitzke@ovgu.de>
2026-04-27 14:52:31 +0000

    5.003 update makefiles

--- src/ms2_version.F90
+++ src/ms2_version.F90
@@ -10,7 +10,7 @@
 
 module ms2_version
 
-  character(*), parameter :: VersionString = '5.002'
-  real(4)                :: ms2VersionNr = 5.002
+  character(*), parameter :: VersionString = '5.003'
+  real(4)                :: ms2VersionNr = 5.003
 
 end module ms2_version 
