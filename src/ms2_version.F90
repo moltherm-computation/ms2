8dc1abb31d0a5eb2d37f69d67e74977dc2a9056b
Isabel Nitzke <isabel.nitzke@ovgu.de>
2026-06-05 10:39:09 +0000

    5.018 Init PartialMolarVolume for OpenSystem

--- src/ms2_version.F90
+++ src/ms2_version.F90
@@ -10,7 +10,7 @@
 
 module ms2_version
 
-  character(*), parameter :: VersionString = '5.017' 
-  real(4)                :: ms2VersionNr = 5.017
+  character(*), parameter :: VersionString = '5.018' 
+  real(4)                :: ms2VersionNr = 5.018
 
 end module ms2_version 
